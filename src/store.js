7e5c771e51f74d5e7ebab0c857d22efad1533f74
Jonas <jonas.mueller.contact@gmail.com>
2018-03-17 13:23:01 +0100

    Added reactotron for logging & debugging

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-import { applyMiddleware, createStore } from 'redux';
-import { createLogger } from 'redux-logger';
+import Reactotron from 'reactotron-react-js'
+import { applyMiddleware, compose } from 'redux';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
 import { persistStore, persistReducer } from 'redux-persist'
@@ -15,8 +15,8 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, reducers);
 const history = createHashHistory();
 const router = routerMiddleware(history);
-const middleware = applyMiddleware(router, thunk, createLogger({}));
-let store = createStore(persistedReducer, middleware);
+const middleware = applyMiddleware(router, thunk);
+const store = Reactotron.createStore(persistedReducer, compose(middleware));
 let persistor = persistStore(store);
 
 export { store, history, persistor};
