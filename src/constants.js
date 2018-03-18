5d323abb0818eb0ed709af0926376c6a6d61e7e4
Jonas <jonas.mueller.contact@gmail.com>
2018-03-18 01:53:04 +0100

    Put constants into constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const ROOT_PATH = '/';
+export const SETTINGS_PATH = '/settings';
+
+export const LOGIN_FORM_NAME = 'login';
+export const SIGNUP_FORM_NAME = 'signup';
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import SettingsPage from './components/SettingsPage';
+import { ROOT_PATH, SETTINGS_PATH } from './constants';
 
 export default () => (
     <App>
-      <Switch>
-          <Route path="/settings" component={SettingsPage} />
-          <Route path="/" component={HomePage} />
-      </Switch>
+        <Switch>
+            <Route path={SETTINGS_PATH} component={SettingsPage} />
+            <Route path={ROOT_PATH} component={HomePage} />
+        </Switch>
     </App>
 );
